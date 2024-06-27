134af5b10a16a3e4c197f2264e54014e8e0b54ff
James Hodgkinson <james@terminaloutcomes.com>
2024-06-27 15:32:25 +1000

    initial commit

---
+++ trainpi/__main__.py
@@ -0,0 +1,35 @@
+# from signal import pause
+import time
+from buildhat import DeviceError, PassiveMotor  # type: ignore
+
+# the train is a "passive" motor - https://buildhat.readthedocs.io/en/latest/buildhat/passivemotor.html
+
+
+def main() -> None:
+    try:
+        print("Hello world")
+        print("Starting Motor")
+
+        motor = PassiveMotor("A")
+
+        try:
+            motor.isconnected()
+        except DeviceError as e:
+            print("DeviceError connecting to motor:", e)
+
+        motor.set_default_speed(30)
+
+        print("Start motor")
+        motor.start()
+        time.sleep(3)
+        print("Stop motor")
+        motor.stop()
+    except Exception as error:
+        print(f"ERROR! {error}")
+        motor.stop()
+
+    # pause()
+
+
+if __name__ == "__main__":
+    main()
